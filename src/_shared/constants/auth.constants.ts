a661891feacca4e9df222b0a2d38b840fe396444
Luan Trajano <luan.trajano@biz.com.br>
2024-02-17 23:33:26 -0300

    feat: created auth and users

---
+++ src/_shared/constants/auth.constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+  secret: 'u!99mg8@z.coLh&&',
+};
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+
+import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { AuthController } from './auth.controller';
+import { jwtConstants } from 'src/_shared/constants/auth.constants';
+
+@Module({
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '10m' },
+    }),
+  ],
+  providers: [AuthService],
+  controllers: [AuthController],
+  exports: [AuthService],
+})
+export class AuthModule {}
