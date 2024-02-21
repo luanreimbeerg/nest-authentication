8bd6eeedaeaf64399cb2c99a65f75122f082e3ad
Luan Trajano <luan.trajano@biz.com.br>
2024-02-21 00:22:31 -0300

    feat: created auth guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 
 import { UsersService } from './users.service';
-import { User } from './entities/user.entity';
+import { User } from '../entities/user.entity';
+import { AuthGuard } from 'src/common/guards/auth.guard';
 
 @Controller()
 export class UsersController {
   constructor(public readonly usersService: UsersService) {}
 
+  @UseGuards(AuthGuard)
+  @Get('/users')
+  async getUserByName(@Query('name') name: string): Promise<User> {
+    console.log(name);
+    return this.usersService.findOneByUsername(name);
+  }
+
+  @UseGuards(AuthGuard)
   @Get('/users')
   async getUsers(): Promise<User[]> {
     return this.usersService.getUsers();
